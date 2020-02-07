59af9d6902c7e0443500c5549093207cd5579308
GabZb <gabrielle.zbaeren@hest.ethz.ch>
2020-02-07 10:39:34 +0100

    task version 5
    With instructions at the beginning (separate script)

---
+++ FLI Application/Assets/Scripts/Emitter/Instructions.cs
@@ -0,0 +1,298 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instructions : MonoBehaviour
+{
+    // reference to timer class
+    public Timer timer;
+
+    private float instructionDuration = 80f;
+
+    // task state machine
+    public enum State
+    {
+        Start,
+        Emitter,
+        Arrows,
+        Basket,
+        EndEstimation,
+        Questions,
+        EndInstructions
+
+    }
+
+    public State state;
+
+    // specific instructions
+    public GameObject Instructions_0;
+    public GameObject Instructions_1;
+    public GameObject Instructions_2;
+    public GameObject Instructions_3;
+    public GameObject Instructions_4;
+    public GameObject Instructions_5;
+    public GameObject Instructions_11;
+
+    public GameObject background;
+
+    public GameObject basket;
+
+    public GameObject emitter;
+    private MeshRenderer _emitterMeshRend;
+
+    public GameObject ball;
+    private MeshRenderer _ballMeshRend;
+
+    //  private Rigidbody _basketRigidbody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+        _emitterMeshRend = emitter.GetComponent<MeshRenderer>();
+        _ballMeshRend = ball.GetComponent<MeshRenderer>();
+
+        // get timer component
+        timer = GetComponent<Timer>();
+
+        // show background
+        background.SetActive(true);
+
+        // show instructions
+        Instructions_0.SetActive(true);
+
+        Instructions_1.SetActive(false);
+        Instructions_2.SetActive(false);
+        Instructions_3.SetActive(false);
+        Instructions_4.SetActive(false);
+        Instructions_5.SetActive(false);
+
+
+        Instructions_11.SetActive(false);
+
+        state = State.Start;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        /// instruction states
+
+        if (state == State.Start)
+        {
+
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                timer.time = instructionDuration;
+                state = State.Emitter;
+            }
+
+        }
+
+        if (state == State.Emitter)
+        {
+
+            // hide background
+            background.SetActive(false);
+
+            // show correct instructions
+            Instructions_0.SetActive(false);
+            Instructions_1.SetActive(true);
+            Instructions_2.SetActive(false);
+            Instructions_3.SetActive(false);
+            Instructions_4.SetActive(false);
+            Instructions_5.SetActive(false);
+
+            if (timer.time <= 73)
+            {
+                // make emitter disappear
+                _emitterMeshRend.enabled = false;
+
+                //make ball disappear
+                _ballMeshRend.enabled = false;
+
+                // make instructions appear
+                Instructions_1.SetActive(false);
+                Instructions_11.SetActive(true);
+            }
+
+
+            if (timer.time <= 65)
+            {
+                state = State.Arrows;
+            }
+
+        }
+
+
+        if (state == State.Arrows)
+        {
+
+
+            // show correct instructions
+            Instructions_0.SetActive(false);
+            Instructions_1.SetActive(false);
+            Instructions_2.SetActive(true);
+            Instructions_3.SetActive(false);
+            Instructions_4.SetActive(false);
+            Instructions_5.SetActive(false);
+
+            Instructions_11.SetActive(false);
+
+
+
+            if (timer.time <= 60)
+            {
+                state = State.Basket;
+            }
+
+        }
+
+        if (state == State.Basket)
+        {
+
+            Instructions_0.SetActive(false);
+            Instructions_1.SetActive(false);
+            Instructions_2.SetActive(true);
+            Instructions_3.SetActive(true);
+            Instructions_4.SetActive(false);
+            Instructions_5.SetActive(false);
+
+            Instructions_11.SetActive(false);
+
+            basket.transform.position = new Vector3(basket.transform.position.x, basket.transform.position.y, basket.transform.position.z);
+
+
+            if (timer.time <= 55)
+
+            {
+                // set basket start position
+                basket.transform.position = new Vector3(2f, basket.transform.position.y, basket.transform.position.z);
+            }
+
+            if (timer.time <= 54)
+
+            {
+                // set basket start position
+                basket.transform.position = new Vector3(4f, basket.transform.position.y, basket.transform.position.z);
+            }
+
+            if (timer.time <= 53)
+
+            {
+                // set basket start position
+                basket.transform.position = new Vector3(6f, basket.transform.position.y, basket.transform.position.z);
+
+            }
+
+            if (timer.time <= 52)
+
+            {
+                // set basket start position
+                basket.transform.position = new Vector3(4f, basket.transform.position.y, basket.transform.position.z);
+            }
+
+            if (timer.time <= 51)
+
+            {
+                // set basket start position
+                basket.transform.position = new Vector3(2f, basket.transform.position.y, basket.transform.position.z);
+            }
+
+            if (timer.time <= 50)
+
+            {
+                // set basket start position
+                basket.transform.position = new Vector3(0f, basket.transform.position.y, basket.transform.position.z);
+            }
+
+            if (timer.time <= 49)
+
+            {
+                // set basket start position
+                basket.transform.position = new Vector3(-2f, basket.transform.position.y, basket.transform.position.z);
+            }
+
+            if (timer.time <= 48)
+
+            {
+                // set basket start position
+                basket.transform.position = new Vector3(-4f, basket.transform.position.y, basket.transform.position.z);
+            }
+
+            if (timer.time <= 47)
+
+            {
+                // set basket start position
+                basket.transform.position = new Vector3(-6f, basket.transform.position.y, basket.transform.position.z);
+            }
+
+
+            if (timer.time <= 45)
+            {
+                state = State.EndEstimation;
+            }
+
+        }
+
+        if (state == State.EndEstimation)
+        {
+
+
+            // show correct instructions
+            Instructions_0.SetActive(false);
+            Instructions_1.SetActive(false);
+            Instructions_2.SetActive(false);
+            Instructions_3.SetActive(false);
+            Instructions_4.SetActive(true);
+            Instructions_5.SetActive(false);
+            Instructions_11.SetActive(false);
+
+            if (timer.time <= 37)
+            {
+                state = State.Questions;
+            }
+
+        }
+
+        if (state == State.Questions)
+        {
+
+
+            // show correct instructions
+            Instructions_0.SetActive(false);
+            Instructions_1.SetActive(false);
+            Instructions_2.SetActive(false);
+            Instructions_3.SetActive(false);
+            Instructions_4.SetActive(false);
+            Instructions_5.SetActive(true);
+            Instructions_11.SetActive(false);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+
+                state = State.EndInstructions;
+
+            }
+
+        }
+
+        if (state == State.EndInstructions)
+        {
+
+            Instructions_0.SetActive(false);
+
+            Instructions_1.SetActive(false);
+            Instructions_2.SetActive(false);
+            Instructions_3.SetActive(false);
+            Instructions_4.SetActive(false);
+            Instructions_5.SetActive(false);
+            Instructions_11.SetActive(false);
+        }
+    }
+}
--- FLI Application/Assets/Scripts/Emitter/SceneController.cs
+++ FLI Application/Assets/Scripts/Emitter/SceneController.cs
@@ -19,40 +19,46 @@ public class SceneController : MonoBehaviour
     // objects set in inspector
     public GameObject emitter;
     private Rigidbody _emitterRigidbody;
+    private MeshRenderer _emitterMeshRend;
 
     public GameObject basket;
     private Rigidbody _basketRigidbody;
 
     public GameObject ball;
     private Rigidbody _ballRigidbody;
+    private MeshRenderer _ballMeshRend;
 
     public GameObject occluder;
     public GameObject background;
 
     // experiment parameters
 
-    //private int subjectID;
-    private int nrTrials = 5; // total number of trials
+    int subjectID = 01; // make it inputable
+    private int nrTrials = 3; // total number of trials
     private float estimationMaxDuration = 10f;
 
     // Lists to define heights, velocities, positions 
 
-    private List<float> emitterHeight = new List<float> { 5f, 4.15f, 3f, 3.15f, 5f };
-    private List<float> emitterVelocity = new List<float> { 5f, -3f, 4f, 2f, -6f };
-    private List<float> emitterStartPosition = new List<float> { -10, 10, -10, -10, 10 }; // influences time it takes for emitter to appear (with velocity)
-    private List<float> emitterDropPosition = new List<float> { -1, 7, -5, 3, 0 };
+    //  private List<float> emitterHeight = new List<float> { 4.15f, 4.15f, 4.15f, 4.15f, 4.15f };
+    //  private List<float> emitterVelocity = new List<float> { 1f, -3f, 5f, 2f, -6f };
+    //   private List<float> emitterStartPosition = new List<float> { -10, 10, -10, -10, 10 }; // influences time it takes for emitter to appear (with velocity)
+    //   private List<float> emitterDropPosition = new List<float> { -1, 7, -5, 3, 0 };
+
+    private List<float> emitterStartPosition = new List<float> { -10, 10, 10, 10, 10, -10, -10, -10, 10, 10, 10, 10, -10, -10, -10, 10, -10, 10, 10, 10, -10, 10, -10, 10, -10, -10, 10, 10, 10, 10, -10, -10, -10, 10, 10, 10, -10, 10, 10, -10, -10, 10, -10, -10, -10, -10, -10, 10, 10, 10, -10, -10, 10, 10, 10, -10, -10, -10, -10, 10 }; // influences time it takes for emitter to appear (with velocity)
+    private List<float> emitterVelocity = new List<float> { 4.5f, -4.5f, -1.5f, -1.5f, -3f, 3f, 1.5f, 4.5f, -4.5f, -1.5f, -1.5f, -3f, 4.5f, 1.5f, 3f, -1.5f, 1.5f, -4.5f, -3f, -1.5f, 4.5f, -4.5f, 4.5f, -4.5f, 3f, 1.5f, -3f, -3f, -4.5f, -3f, 1.5f, 3f, 4.5f, -3f, -1.5f, -4.5f, 1.5f, -3f, -1.5f, 3f, 3f, -4.5f, 1.5f, 4.5f, 4.5f, 4.5f, 3f, -1.5f, -4.5f, -4.5f, 4.5f, 3f, -3f, -1.5f, -3f, 1.5f, 1.5f, 1.5f, 3f, -3f };
+    private List<float> emitterHeight = new List<float> { 1f, 5f, 5f, 1f, 3f, 5f, 3f, 5f, 3f, 5f, 1f, 3f, 5f, 5f, 3f, 3f, 5f, 5f, 5f, 5f, 5f, 5f, 5f, 5f, 1f, 5f, 5f, 3f, 1f, 5f, 1f, 3f, 3f, 5f, 1f, 5f, 3f, 3f, 3f, 5f, 5f, 3f, 1f, 3f, 1f, 5f, 3f, 5f, 3f, 1f, 3f, 1f, 1f, 1f, 3f, 1f, 1f, 5f, 5f, 1f };
+    private List<float> emitterDropPosition = new List<float> { -3, 0, -3, 2, 1, 1, 1, 2, -0, -2, -3, -0, -2, -2, -1, -1, 2, 2, -1, 1, 0, 1, 3, -2, 2, 3, -2, 2, 1, -3, -2, -2, 0, 1, 3, -3, 0, 0, 0, -1, 2, -2, -1, 3, -1, -1, 1, 2, -3, 3, 2, -3, -2, 1, -1, 3, -3, -1, 3, 3 };
 
     // parameter tracking
     private int trialNr; // current trial number
-    private float estimationLocation;
-    private float estimationDuration;
 
     // reference to timer class
     public Timer timer;
 
-    // state machine
+    // task state machine
     public enum State
     {
+        Wait,
         Setup,
         GetReady,
         TransitionToTask,
@@ -72,7 +78,7 @@ public class SceneController : MonoBehaviour
     // tracking parameters
     // private int dropCount; // ?
 
-    // instructions
+    // general instructions
     public GameObject startInstruction;
     public GameObject restInstruction;
     public GameObject endInstruction;
@@ -84,25 +90,27 @@ public class SceneController : MonoBehaviour
     {
 
         // set the path
-        GlobalParameters.Path = Application.dataPath + "/Responses.txt";
+        GlobalParameters.Path = Application.dataPath + "/Data/" + subjectID + "_responses.txt";
 
         // get reference to scripts
         // dropController = GetComponent<DropController>();
 
-        // get emitter rigidbody 
+        // get emitter rigidbody & mesh renderer
         _emitterRigidbody = emitter.GetComponent<Rigidbody>();
+        _emitterMeshRend = emitter.GetComponent<MeshRenderer>();
 
         // get basket rigidbody 
         _basketRigidbody = basket.GetComponent<Rigidbody>();
 
-        // get ball rigidbody 
+        // get ball rigidbody & mesh renderer
         _ballRigidbody = emitter.GetComponent<Rigidbody>();
+        _ballMeshRend = ball.GetComponent<MeshRenderer>();
 
         // get timer component
         timer = GetComponent<Timer>();
 
         // create .txt file with date and time
-        File.WriteAllText(GlobalParameters.Path, "\n\n\n" + "Date and time: " + DateTime.Now.ToString("yyyyMMdd - HHmmss") + "\n\n");
+        File.WriteAllText(GlobalParameters.Path, "\n\n\n" + "Date and time: " + DateTime.Now.ToString("yyyyMMdd - HHmmss") + "\n\n" + "trial side height velocity trueLandingPosition estimatedLandinPosition difference responseTime");
 
         // initialise add velocity to true 
         GlobalParameters.AddVelocity = true; // ?
@@ -110,8 +118,8 @@ public class SceneController : MonoBehaviour
         // initialize trialNr to 1
         trialNr = 1;
 
-        // initialise state
-        state = State.Setup;
+        // initialise state 
+        state = State.Wait;
 
     }
 
@@ -120,20 +128,46 @@ public class SceneController : MonoBehaviour
     private void Update()
     {
 
-        if (state == State.Setup)
-        {
+        
 
-            // show start instruction
-            startInstruction.SetActive(true);
+        /// task states:
+        
+        //waits for instructions to be over
+        if (state == State.Wait)
+        {
+            // hide start instruction
+            startInstruction.SetActive(false);
 
             // hide estimation instruction
             estimationInstruction.SetActive(false);
 
+            // hide end instruction
+            endInstruction.SetActive(false);
+
             // hide rest instruction
             restInstruction.SetActive(false);
 
-            // hide end instruction
-            endInstruction.SetActive(false);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                state = State.Setup;
+            }
+        }
+
+        if (state == State.Setup)
+        {
+
+
+            //            //make emitter visible
+            _emitterMeshRend.enabled = true;
+
+//            //make ball visible
+            _ballMeshRend.enabled = true;
+
+            // show start instruction
+            startInstruction.SetActive(true);
+
+            // hide rest instruction
+            restInstruction.SetActive(false);
 
             // hide background
             background.SetActive(false);
@@ -202,7 +236,7 @@ public class SceneController : MonoBehaviour
             //if (Mathf.Approximately(emitter.transform.position.x,_emitterDropPosition))
 
 
-            // if position emitter is at drop point, make occluder appear
+            // if position emitter is at drop point,
             if (emitterVelocity[trialNr - 1] > 0)
                {
 
@@ -211,8 +245,21 @@ public class SceneController : MonoBehaviour
 
                       state = State.TransitionToEstimation;
 
-                  }
-               }
+                    //make emitter and ball freeze
+                    GlobalParameters.EmitterVelocity = -emitterVelocity[trialNr - 1];
+                    _emitterRigidbody.AddForce(new Vector3(GlobalParameters.EmitterVelocity, 0f, 0f), ForceMode.VelocityChange);
+
+                    // make emitter disappear
+                    _emitterMeshRend.enabled = false;
+
+////                    //make ball disappear
+                   _ballMeshRend.enabled = false;
+
+ //                  occluder.SetActive(true);
+
+                    
+                }
+            }
 
             else
 
@@ -223,13 +270,27 @@ public class SceneController : MonoBehaviour
 
                       state = State.TransitionToEstimation;
 
-                  }
+                    //make emitter and ball freeze
+                    GlobalParameters.EmitterVelocity = - emitterVelocity[trialNr - 1];
+                    _emitterRigidbody.AddForce(new Vector3(GlobalParameters.EmitterVelocity, 0f, 0f), ForceMode.VelocityChange);
+
+                    // make emitter disappear
+                    _emitterMeshRend.enabled = false;
+
+////                   //make ball disappear
+                    _ballMeshRend.enabled = false;
+
+ //                   occluder.SetActive(true);
+
+
+                }
                }
 
         }
 
         if (state == State.TransitionToEstimation)
         {
+
             // hide start instruction
             startInstruction.SetActive(false);
 
@@ -237,7 +298,7 @@ public class SceneController : MonoBehaviour
             restInstruction.SetActive(false);
 
             // occlude screen
-            occluder.SetActive(true);
+//            occluder.SetActive(true);
 
             // set timer to duration of next state
             timer.time = estimationMaxDuration;
@@ -250,11 +311,11 @@ public class SceneController : MonoBehaviour
         if (state == State.Estimation)
         {
 
-            if (timer.time <= 9.5)
-            {
+         //   if (timer.time <= 9.5)
+         //   {
                 // show estimation instruction
                 estimationInstruction.SetActive(true);
-            }
+         //   }
 
             // allow basket to be moved
             Vector3 basketPosition = basket.transform.position;
@@ -366,9 +427,10 @@ public class SceneController : MonoBehaviour
     {
 
         // Content of the file
-        string content = "trial : " + trialNr + "\n" + "emitter start location : " + side + "\n" + "correct location : " + LandingPosition + "\n" + "estimated location : "
-            + basket.transform.position.x + "\n" + "difference : " + Difference + "\n" + "response time : " + ResponseTime + "\n\n";
+    //    string content = "trial : " + trialNr + "\n" + "emitter start location : " + side + "\n" + "correct location : " + LandingPosition + "\n" + "estimated location : "
+    //        + basket.transform.position.x + "\n" + "difference : " + Difference + "\n" + "response time : " + ResponseTime + "\n\n";
 
+        string content = "\n\n" + trialNr + " " + (emitterHeight[trialNr - 1] + 3.6f - 0.5f) + " " + side + " " + emitterVelocity[trialNr - 1] + " " + LandingPosition + " " + basket.transform.position.x + " " + Difference + " " + ResponseTime;
 
         // Add content to existing file
         File.AppendAllText(GlobalParameters.Path, content); //writealltext will replace text
@@ -381,7 +443,9 @@ public class SceneController : MonoBehaviour
         double gravity = 9.81;
 
         // equation for vertical part (- basket position because it is negative)
-        double travelTime = Math.Sqrt((2 * (emitterHeight[trialNr - 1] - basket.transform.position.y)) / gravity);
+        // double travelTime = Math.Sqrt((2 * (emitterHeight[trialNr - 1] - basket.transform.position.y)) / gravity);
+
+        double travelTime = Math.Sqrt((2 * (emitterHeight[trialNr - 1] + 3.6f - 0.5f)) / gravity); // - 3.6 = position of basket relative to 0, 0.5 = position of ball relative to emitter
 
         // equation for horizontal part
         double horDistance = ((emitterVelocity[trialNr - 1] * travelTime)); // horizontal distance travelled
